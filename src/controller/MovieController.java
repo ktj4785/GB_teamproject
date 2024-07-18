4ae960265a35171517e4f0db7ab43a37cff70eb9
unknown <nong8236@naver.com>
2024-07-18 17:42:17 +0900

    두번째 업데이트에용

--- src/controller/MovieController.java
+++ src/controller/MovieController.java
@@ -1,5 +1,12 @@
 package controller;
 
-public class MovieController {
+import model.dao.MovieDAO;
 
+public class MovieController {
+	
+	//영화 목록 불러오기
+	public void getList(int choice) {
+		MovieDAO mdao = new MovieDAO();
+		return mdao.getlist();
+	}
 }
--- src/view/MovieListView.java
+++ src/view/MovieListView.java
@@ -1,7 +1,13 @@
 package view;
 
+import java.util.Scanner;
+
+
 public class MovieListView {
-	public MovieListView() {
-		
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		MovieController controller = new MovieController();
+		System.out.println("1. 평점 순으로 보기\n2. 이름 순으로 보기");
+		int choice = sc.nextInt();	
 	}
 }
