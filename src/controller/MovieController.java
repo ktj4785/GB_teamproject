3499f00f1b3b1c31d72dfda1334f8c08386ec4d6
unknown <nong8236@naver.com>
2024-07-24 17:43:31 +0900

    moviesearch

--- src/controller/MovieController.java
+++ src/controller/MovieController.java
@@ -1,5 +1,31 @@
 package controller;
 
+
+
+import java.util.ArrayList;
+
+import model.dao.MovieDAO;
+import model.dto.MovieDTO;
+
 public class MovieController {
 
+	// 영화 목록 불러오기
+	public ArrayList<MovieDTO> getList(int choice) {
+		MovieDAO mdao = new MovieDAO();
+		return mdao.getlist(choice);
+	}
+
+	public ArrayList<MovieDTO> searchMovieName(String ans) {
+		MovieDAO mdao = new MovieDAO();
+		return mdao.getMovieName(ans);
+	}
+	public ArrayList<MovieDTO> searchActorName(String ans) {
+		MovieDAO mdao = new MovieDAO();
+		return mdao.getActorName(ans);
+	}
+
+	public ArrayList<MovieDTO> searchDirectorName(String ans) {
+		MovieDAO mdao = new MovieDAO();
+		return mdao.getDirectorName(ans);
+	}
 }
--- src/model/dao/MovieDAO.java
+++ src/model/dao/MovieDAO.java
@@ -1,5 +1,193 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import model.DBConnection;
+import model.dto.MovieDTO;
+
+
+
 public class MovieDAO {
+	Connection conn;
+	PreparedStatement ps;
+	ResultSet rs;
+
+	public MovieDAO() {
+		conn = DBConnection.getConnection();
+	}
+
+	public ArrayList<MovieDTO> getlist(int choice) {
+
+		ArrayList<MovieDTO> list = new ArrayList<>();
+
+		String[] column = {"score desc","movieName"};
+		String sql = "select \r\n"
+				+ "mt.movieId,\r\n"
+				+ "mt.movieName,\r\n"
+				+ "mt.director,\r\n"
+				+ "mt.runningTime,\r\n"
+				+ "mt.genre,\r\n"
+				+ "format(avg(r.grade),1)as score\r\n"
+				+ "from movietest mt\r\n"
+				+ "	join reviewtest r using(movieId)\r\n"
+				+ "group by mt.movieId,\r\n"
+				+ "mt.movieName,\r\n"
+				+ "mt.director,\r\n"
+				+ "mt.runningTime,\r\n"
+				+ "mt.genre\r\n"
+				+ "order by "+column[choice-1];
+
+		try {
+			ps = conn.prepareStatement(sql);
+//			ps.setString(1, column[choice]);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				MovieDTO movielist = new MovieDTO(
+						rs.getInt("movieId"), 
+						rs.getString("movieName"),
+						rs.getString("director"), 
+						rs.getString("runningTime"), 
+						rs.getString("genre"),
+						rs.getDouble("score"));
+				list.add(movielist);
+			}
+		} catch (SQLException e) {
+			System.err.println(e);
+		}
+		return list;
+	}
+
+	
+	
+	public ArrayList<MovieDTO> getMovieName(String ans) {
+		
+		ArrayList<MovieDTO> list = new ArrayList<>();
+		String sql = "select \r\n"
+				+ "mt.movieId,\r\n"
+				+ "mt.movieName,\r\n"
+				+ "mt.director,\r\n"
+				+ "mt.runningTime,\r\n"
+				+ "mt.genre,\r\n"
+				+ "format(avg(r.grade),1)as score\r\n"
+				+ "from movietest mt\r\n"
+				+ "	join reviewtest r using(movieId)\r\n"
+				+ "where mt.moviename like ? \r\n"
+				+ "group by mt.movieId,\r\n"
+				+ "mt.movieName,\r\n"
+				+ "mt.director,\r\n"
+				+ "mt.runningTime,\r\n"
+				+ "mt.genre;";
+		
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, "%" + ans + "%");
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				MovieDTO movielist = new MovieDTO(
+						rs.getInt("movieId"), 
+						rs.getString("movieName"),
+						rs.getString("director"), 
+						rs.getString("runningTime"), 
+						rs.getString("genre"),
+						rs.getDouble("score"));
+				list.add(movielist);
+			}
+		} catch (SQLException e) {
+			System.err.println(e);
+		}
+		return list;
+	}
+
+	public ArrayList<MovieDTO> getActorName(String ans) {
+
+		ArrayList<MovieDTO> list = new ArrayList<>();
+		String sql = "select\r\n"
+				+ "ma.movieId,\r\n"
+				+ "ma.movieName,\r\n"
+				+ "ma.director,\r\n"
+				+ "ma.runningTime,\r\n"
+				+ "ma.genre,\r\n"
+				+ "format(avg(r.grade),1)as score\r\n"
+				+ "from(select \r\n"
+				+ "mt.movieId,\r\n"
+				+ "mt.movieName,\r\n"
+				+ "mt.director,\r\n"
+				+ "mt.runningTime,\r\n"
+				+ "mt.genre,\r\n"
+				+ "a.actorName\r\n"
+				+ "from movietest mt\r\n"
+				+ "	join actortest a using(movieId)\r\n"
+				+ "where actorName like ?) ma\r\n"
+				+ "join reviewtest r using(movieId)\r\n"
+				+ "group by ma.movieId,\r\n"
+				+ "ma.movieName,\r\n"
+				+ "ma.director,\r\n"
+				+ "ma.runningTime,\r\n"
+				+ "ma.genre\r\n"
+				+ "order by ma.movieName;";
+		
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, "%" + ans + "%");
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				MovieDTO movielist = new MovieDTO(
+						rs.getInt("movieId"), 
+						rs.getString("movieName"),
+						rs.getString("director"), 
+						rs.getString("runningTime"), 
+						rs.getString("genre"),
+						rs.getDouble("score"));
+				list.add(movielist);
+			}
+		} catch (SQLException e) {
+			System.err.println(e);
+		}
+		return list;
+	}
+	
+	
+	public ArrayList<MovieDTO> getDirectorName(String ans) {
+		
+		ArrayList<MovieDTO> list = new ArrayList<>();
+		String sql = "select \r\n"
+				+ "mt.movieId,\r\n"
+				+ "mt.movieName,\r\n"
+				+ "mt.director,\r\n"
+				+ "mt.runningTime,\r\n"
+				+ "mt.genre,\r\n"
+				+ "format(avg(r.grade),1)as score\r\n"
+				+ "from movietest mt\r\n"
+				+ "	join reviewtest r using(movieId)\r\n"
+				+ "where mt.director like ? \r\n"
+				+ "group by mt.movieId,\r\n"
+				+ "mt.movieName,\r\n"
+				+ "mt.director,\r\n"
+				+ "mt.runningTime,\r\n"
+				+ "mt.genre;";
+		
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, "%" + ans + "%");
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				MovieDTO movielist = new MovieDTO(
+						rs.getInt("movieId"), 
+						rs.getString("movieName"),
+						rs.getString("director"), 
+						rs.getString("runningTime"), 
+						rs.getString("genre"),
+						rs.getDouble("score"));
+				list.add(movielist);
+			}
+		} catch (SQLException e) {
+			System.err.println(e);
+		}
+		return list;
+	}
 
 }
